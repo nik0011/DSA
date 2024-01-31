943d0d92faf80fddfa5aaec29278f08f66469e6f
nik001 <nikhiltakle055@gmail.com>
2024-01-31 22:37:55 +0530

    .

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -14,7 +14,9 @@
 //         return 0;
 // }
 
-
+//for 111
+// 222
+// 333
 // #include<iostream>
 // using namespace std;
 // int main(){
@@ -28,3 +30,23 @@
 //     }
 //         return 0;
 // }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    // for printing 1234 and same in the second row...
+    // for (int i = 1; i <= n; i++){
+    //     for (int j=1; j <=n; j++){
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for printing 123
+    //456
+    //789
+    
+        return 0;
+}
