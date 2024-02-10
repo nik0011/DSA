b1c3e9c90f8a5cf869ebc2d29b1a7cb8e2ab9cd3
nik001 <nikhiltakle055@gmail.com>
2024-02-11 01:08:38 +0530

    -

--- Decimal-Binary/db.cpp
+++ Decimal-Binary/db.cpp
@@ -1,3 +1,4 @@
+//decimal to binary....
 // #include<iostream>
 // #include<math.h>
 // using namespace std;
@@ -13,6 +14,32 @@
 //         n = n >> 1;
 //         i++;
 //     }
-//     cout << ans;
+//     cout<<ans;
 //     return 0;
 // }
+
+//negative decimal no to binary
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     int i;
+//     int ans;
+//         int ones = ~n;
+//         int one = ones + 1;
+//         int tows = ~one;
+//         int two = tows + 1;
+//         while(two!=0){
+//             int bit = two & 1;
+//             ans = (bit * pow(10, i)) + ans;
+//             two = two >> 1;
+//             i++;
+//         }
+//         cout << ans;
+
+//         cout << ans;
+//         return 0;
+// }
+
