060f19676db43ef1b3d4e0f4a57c8f9009b6ffe8
nik001 <nikhiltakle055@gmail.com>
2024-02-11 19:45:05 +0530

    -

--- Decimal-Binary/db.cpp
+++ Decimal-Binary/db.cpp
@@ -44,22 +44,62 @@
 // }
 
 //Binary to decimal.....
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     int i=0;
+//     int ans=0;
+//     while(n!=0){
+//         int digit = n % 10;
+//         if(digit==1){
+//             ans = ans + pow(2, i);
+//         }
+//         i++;
+//         n = n/10;
+//     }
+//     cout << ans;
+//     return 0;
+// }
+
+
+//reversing a number, if it cross the rang then print 0
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     int ans = 0;
+//     while(n!=0){
+//         int digit = n % 10;
+//         if((ans>INT_MAX/10)||(ans<INT_MIN/10)){
+//             cout << "0";
+//         }
+//         ans = (ans * 10) + digit;
+//         n = n / 10;
+//     }
+//     cout << ans;
+//     return 0;
+// }
+
+// complement of base 10
 #include<iostream>
-#include<math.h>
 using namespace std;
 int main(){
     int n;
     cin >> n;
-    int i=0;
-    int ans=0;
-    while(n!=0){
-        int digit = n % 10;
-        if(digit==1){
-            ans = ans + pow(2, i);
-        }
-        i++;
-        n = n/10;
+    int m = n;
+    int mask = 0;
+    if(n==0){
+        cout << "1";
     }
+    while(m!=0){
+        m = m >> 1;
+        mask = (mask << 1) | 1;
+    }
+    int ans = (~n) & mask;
     cout << ans;
     return 0;
 }
