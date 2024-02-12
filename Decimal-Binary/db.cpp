2ea19e41f20633eb512dfe09848d8ae1675f4986
nik001 <nikhiltakle055@gmail.com>
2024-02-12 11:53:24 +0530

    -

--- Decimal-Binary/db.cpp
+++ Decimal-Binary/db.cpp
@@ -85,21 +85,21 @@
 // }
 
 // complement of base 10
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    int m = n;
-    int mask = 0;
-    if(n==0){
-        cout << "1";
-    }
-    while(m!=0){
-        m = m >> 1;
-        mask = (mask << 1) | 1;
-    }
-    int ans = (~n) & mask;
-    cout << ans;
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     int m = n;
+//     int mask = 0;
+//     if(n==0){
+//         cout << "1";
+//     }
+//     while(m!=0){
+//         m = m >> 1;
+//         mask = (mask << 1) | 1;
+//     }
+//     int ans = (~n) & mask;
+//     cout << ans;
+//     return 0;
+// }
