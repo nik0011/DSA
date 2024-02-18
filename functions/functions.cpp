28f26f140365a28f689eb6da7f1d4313856f8bb8
nik001 <nikhiltakle055@gmail.com>
2024-02-18 18:02:54 +0530

    -

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -16,26 +16,50 @@
 //     return 0;
 // }
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int fact(int n){
-    int ans = 1;
-    for (int i = 1; i <= n; i++){
-         ans = ans * i;
+// int fact(int n){
+//     int ans = 1;
+//     for (int i = 1; i <= n; i++){
+//          ans = ans * i;
        
-    } 
-    return ans;
-}
-int nCr(int n, int r){
-    int num = fact(n);
-    int denom = fact(r) * fact(n - r);
-    return num / denom;
-}
-int main(){
-    int n, r;
-    cin >> n>> r;
-    int answer = nCr(n, r);
-    cout<<answer;
-    return 0;
-}
+//     } 
+//     return ans;
+// }
+// int nCr(int n, int r){
+//     int num = fact(n);
+//     int denom = fact(r) * fact(n - r);
+//     return num / denom;
+// }
+// int main(){
+//     int n, r;
+//     cin >> n>> r;
+//     int answer = nCr(n, r);
+//     cout<<answer;
+//     return 0;
+// }
+
+// for calculating total no of 1 bits in 2 no 
+// #include<iostream>
+// using namespace std;
+// int setbits(int n){
+//     int count = 0;
+//     while(n!=0){
+//         if(n&1){
+//             count++;
+//         }
+//         n = n >> 1;
+//     }
+//     return count;
+// }
+
+// int main(){
+//     int a, b;
+//     cin >> a >> b;
+//     int a1 = setbits(a);
+//     int b1 = setbits(b);
+//     int ans = a1 + b1;
+//     cout << ans;
+//     return 0;
+// }
