f389da09d2520221c452ba51fc6f4fbe71566244
nik001 <nikhiltakle055@gmail.com>
2024-02-19 15:29:29 +0530

    -

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -1,1 +1,32 @@
- 
+// for intitializing array with 1 default value
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int size = 5;
+//     int defaultValue = 5;
+//     int myArray[size];
+//     for (int i = 0; i < size; i++) {
+//         myArray[i] = defaultValue;
+//         cout << myArray[i] << " ";
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int printarray(int arr[],int size){
+    for (int i = 0; i < size;i++){
+        cout << arr[i] << " ";
+    }
+   
+    return 0;
+}
+int main() {
+    int firstarr[10] = {1, 2};
+    int size = 10;
+    int arraysize = sizeof(firstarr) / sizeof(int);
+    cout << arraysize;
+    printarray(firstarr, size);
+    
+    return 0;
+}
