7f89d8a9373964589f42d7c9000a3993408cec8a
nik001 <nikhiltakle055@gmail.com>
2024-02-19 20:23:35 +0530

    -

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -65,3 +65,25 @@
 //     cout << "min value = " << getmin(arr, n)<<endl;
 //     return 0;
 // }
+
+// for calc sum of all element...
+#include <iostream>
+using namespace std;
+int totalsum(int arr[], int n){
+    int sum=0;
+    for (int i = 0; i < n; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+int main() {
+    int arr[100];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << totalsum(arr, n);
+
+    return 0;
+}
