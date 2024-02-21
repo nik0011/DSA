6179628f259119e68fa483e0d549a20241ab80e9
nik001 <nikhiltakle055@gmail.com>
2024-02-21 23:40:52 +0530

    -

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -113,3 +113,30 @@
 //     }
 //     return 0;
 // }
+
+//reverse the array....
+#include <iostream>
+using namespace std;
+void reverse(int arr[],int n){
+    int start = 0;
+    int end = n - 1;
+    while(start<=end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+void printarray(int arr[], int n){
+    for (int i = 0; i < n; i++){
+        cout << arr[i]<<" " ;
+    }
+}
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int brr[5] = {6, 7, 8, 9, 10};
+    reverse(arr, 5);
+    reverse(brr, 5);
+    printarray(arr, 5);
+    printarray(brr, 5);
+    return 0;
+}
